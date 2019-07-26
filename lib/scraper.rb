eea401efc0f5b9324cf9de59af3c076944d43eea
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 02:54:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
       binding.pry
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
-      course.description
+      course.description = post.css("p").text
     end
   end
 
